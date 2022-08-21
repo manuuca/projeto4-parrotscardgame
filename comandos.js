24897fb4f48f1adbc2aeb3b76400a1fdf51b29a4
manuela <manuela.c.caetano@gmail.com>
2022-08-20 23:27:05 -0300

    <Feat> Seletor de numCads e disposição cartas

--- comandos.js
+++ comandos.js
@@ -1,16 +1,16 @@
 function startGame (){
-    let numCards = 4 //prompt("Com quantas cartas você gostaria de jogar? (favor escolher um número entre 4 e 14)")
+    let numCards = 10 //prompt("Com quantas cartas você gostaria de jogar? (favor escolher um número entre 4 e 14)")
  while (numCards%2 || numCards < 4 || numCards > 14){
-    numCards = prompt("Com quantas cartas você gostaria de jogar? (favor escolher um número entre 4 e 14)");
+    numCards = prompt("Com quantas cartas você gostaria de jogar? (Por favor, escolha um número entre 4 e 14)");
  }
+
    const cards = [];
    for (let index = 0; index < numCards; index++) {
       cards.push(
          `<div class="card card${index}">
-               <img class="front-side hidden" src="image/front.png" />
-               <img class="turned-side not-hidden"  src="image/carta${(index+1)}.gif" />                
-         </div>`
-         
+               <img class="front-side hidden" src="image/front.png"/>
+               <img class="turned-side not-hidden"  src="image/carta${(index+1)}.gif"/>                
+         </div>`        
       )      
    }
 
@@ -19,8 +19,6 @@ function startGame (){
    function comparador() { 
       return Math.random() - 0.5; 
    }
-
-console.log(cards);
 
  const divContainerCards = document.querySelector(".container-cards")
    for (let i = 0; i < numCards ; i++){
